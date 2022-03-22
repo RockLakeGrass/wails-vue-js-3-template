ed48fbbdab0083b241b29860733a9aca451fb73c
Eric-WangHanming <68141745+Eric-WangHanming@users.noreply.github.com>
2022-03-22 20:09:20 +0800

    first update

---
+++ frontend/src/App.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="header">
+    <div style="height: 25px;width: 100%;display: flex;justify-content: right" data-wails-drag>
+      <div data-wails-no-drag style="display: flex">
+        <div class="hover1" style="width: 35px;height: 25px;display: flex;justify-content: center;align-items: center;"
+             @click="miniWindowBtn">
+          <LineOutlined style="color: aliceblue;font-size: 15px"/>
+        </div>
+        <div class="hover1" style="width: 35px;height: 25px;display: flex;justify-content: center;align-items: center;"
+             @click="maxWindowBtn">
+          <RetweetOutlined v-if="maxStatus" style="color: aliceblue;font-size: 12px"/>
+          <BorderOutlined v-else style="color: aliceblue;font-size: 12px"/>
+        </div>
+        <div class="hover2"
+             style="width: 35px;height: 25px;display: flex;justify-content: center;align-items: center;border-radius:0 6px 0 0"
+             @click="closeBtn">
+          <CloseOutlined style="color: aliceblue;font-size: 15px"/>
+        </div>
+      </div>
+    </div>
+    <Home/>
+  </div>
+</template>
+
+<script>
+import {BorderOutlined, CloseOutlined, LineOutlined,RetweetOutlined} from "@ant-design/icons-vue";
+import Home from "@/components/Home.vue";
+
+export default {
+  components: {
+    CloseOutlined,
+    BorderOutlined,
+    LineOutlined,
+    RetweetOutlined,
+    Home
+  },
+  data() {
+    return{
+      maxStatus: false,
+    }
+  },
+  methods: {
+    miniWindowBtn() {
+      window.runtime.WindowMinimise();
+    },
+    maxWindowBtn() {
+      if (this.maxStatus){
+        window.runtime.WindowUnmaximise();
+        this.maxStatus = false;
+      }else{
+        window.runtime.WindowMaximise();
+        this.maxStatus = true;
+      }
+    },
+    closeBtn() {
+      window.runtime.Quit();
+    }
+  }
+}
+</script>
+<style>
+html, body, #app {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  width: 100%;
+  background-color: rgba(255, 255, 255, 0);
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+
+.header {
+  height: 100%;
+  width: 100%;
+  background: #282b3e;
+  border-radius: 6px;
+  border: 0;
+}
+
+.hover1:hover {
+  background: #a3a3a3;
+}
+
+.hover2:hover {
+  background: #b20000;
+}
+</style>
---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="home">
+    <div style="width: 250px;padding-top: 50px">
+      <div style="width: 100%;display: flex;justify-content: center">
+        <h1 style="color: aliceblue">{{ title }}</h1>
+      </div>
+      <div style="margin-bottom: 15px;">
+        <a-input id="ipt" @pressEnter="Greet"/>
+      </div>
+      <div style="justify-content: center;display: flex">
+        <a-button @click="Greet">提交</a-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {},
+  data() {
+    return{
+      title: "Hello World",
+    }
+  },
+  methods: {
+    Greet() {
+      window.go.main.App.Greet(document.getElementById("ipt").value).then(res => {
+        this.title = res;
+      });
+    },
+  },
+}
+</script>
+
+<style scoped>
+.home {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+}
+</style>
